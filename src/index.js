e84faea6fcb220e7648474f2477de53cc963e435
elygo <el.uzb93@gmail.com>
2020-10-18 02:51:34 +0500

    init: add a morse_decoder function
    This function takes expressions which include zeros, tens and elevens converts them - from morse to words.

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  var sliced = '';
+  var final = '';
+    for (var i = 10; i <= expr.length; i += 10 ) {
+      //declare empty decoded (means from 10,11 to morse)
+      var decoded = '';
+      sliced = expr.slice(i-10, i)
+
+      if (sliced === '**********') {
+        final = final + ' '
+      } else {
+        // check 10 or 11 with 1 and 0 separately by steps 2
+            for (var j=0; j < sliced.length; j += 2){
+                    if (sliced.charAt(j) === '1' && sliced.charAt(j+1) === '0'){
+                      decoded = decoded + '.'
+                    } else if (sliced.charAt(j) === '1' && sliced.charAt(j+1) === '1'){
+                      decoded = decoded + '-'
+                    } else {}
+            }
+      }
+      //check if morse table includes this expression
+      if (decoded in MORSE_TABLE){
+         final = final + MORSE_TABLE[decoded]
+      }
+
+    }
+  return final;
 }
 
 module.exports = {
